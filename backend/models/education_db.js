6603bc28ac63c9cb3446fc6d0c13e265b0a9172e
harshpatel2212 <harshup2212@gmail.com>
2022-09-24 03:07:56 +0530

    DB added

---
+++ backend/models/education_db.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const EduSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  tagField: [
+    {
+      type: String,
+    },
+  ],
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  day: {
+    type: String,
+    required: true,
+  },
+  time: {
+    timestamps: true,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  contactNo: {
+    type: Number,
+    required: true,
+  },
+});
+
+const admin = new mongoose.model("register", EduSchema);
+module.exports = admin;
